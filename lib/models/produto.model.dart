89aa22e8f750e7fa7f9b35f38d5389d096f8156d
JhonnyAraujo <jhonny.a.santana2002@gmail.com>
2026-03-08 15:51:50 -0300

    feat: adiciona model Produto para transição de dados

---
+++ lib/models/produto.model.dart
@@ -0,0 +1,13 @@
+class Produto {
+  String? titulo;
+  double? preco;
+  double? valorParcelas;
+  String? cor;
+
+  Produto({
+    required this.titulo,
+    required this.preco,
+    required this.valorParcelas,
+    required this.cor,
+  });
+}
